3bfc48cb69ed6af4da2c4d0fd29a35873d8aa10f
echobool <ljy518338@163.com>
2019-12-03 17:13:58 +0800

    添加了默认服务

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"github.com/micro/micro/cmd"
+	"github.com/micro/micro/plugin"
+	"github.com/micro/go-plugins/micro/index"
 )
+
+func init() {
+	plugin.Register(index.NewPlugin())
+}
 
 func main() {
 	cmd.Init()
